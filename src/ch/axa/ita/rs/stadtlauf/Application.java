89992b551b5231eef85a0aba8faf62a479ad2cb5
Raphael Suter <raphael.werner.suter@gmail.com>
2021-01-18 13:04:07 +0100

    Set up project

---
+++ src/ch/axa/ita/rs/stadtlauf/Application.java
@@ -0,0 +1,12 @@
+package ch.axa.ita.rs.stadtlauf;
+
+public class Application {
+    public Application() {
+        super();
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        new Application();
+    }
+}
